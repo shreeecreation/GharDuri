7cdbc1b655af9d1ef37b40231b0d7e8d03121833
shreeecreation <shreeecreation720#gmail.com>
2023-05-20 12:31:03 +0545

    Feat : Start Question Page

---
+++ lib/src/app/screens/Questions/chekbox_questions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CheckBoxQuestions extends StatelessWidget {
+  const CheckBoxQuestions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
